8f11b84bec39ba7670a0b77625a71ef448e9ac6d
Marcus Lau <lau.marcus76@gmail.com>
2020-04-25 00:45:56 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -72,7 +72,14 @@ end
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
-  
+  bbq = holiday_hash.collect do |season, holiday|
+    holiday.collect do |holiday, supply|
+      if supply == "BBQ" 
+        holiday
+      end
+    end
+  end
+  bbq.flatten
 end
 
 
